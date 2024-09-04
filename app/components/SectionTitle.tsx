18a35ef7be71434a84f3f033743ba5e83661a446
Bulut Yerli <bulutyerli@gmail.com>
2024-09-04 13:05:11 +0300

    change h1 to h2 on projects title component

--- app/components/SectionTitle.tsx
+++ app/components/SectionTitle.tsx
@@ -7,11 +7,11 @@ export default function SectionTitle({ title }: { title: string }) {
         <div className="w-full border-t border-zinc-200 dark:border-zinc-600" />
       </div>
       <div className="relative flex justify-center">
-        <h1
+        <h2
           className={`${spartan.className} text-2xl md:text-3xl xl:text-4xl tracking-tighter font-semibold text-center bg-white dark:bg-zinc-900 pr-3 leading-6 text-zinc-700 dark:text-zinc-300`}
         >
           {title}
-        </h1>
+        </h2>
       </div>
     </div>
   );
