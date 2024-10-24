205c7e01a7eca7f505f3d0810bdf0e394d7f020f
bulut yerli <bulutyerli@gmail.com>
2024-10-24 14:44:29 +0300

    add tests for container component

---
+++ app/components/__tests__/Container.test.tsx
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react';
+import clsx from 'clsx';
+import Container from '../Container';
+
+describe('container render test suite', () => {
+  it('should render container with child and class without error', () => {
+    const testClass = 'test-class';
+    const testChildren = <h1>test</h1>;
+
+    const { container, getByRole } = render(
+      <Container className={testClass}>{testChildren}</Container>
+    );
+
+    expect(container.firstChild).toHaveClass('test-class');
+    expect(getByRole('heading', { name: /test/i })).toBeInTheDocument();
+  });
+});
