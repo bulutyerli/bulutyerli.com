777b8194ec38aebb5f3d8053172a679b4d0ba76b
bulut yerli <bulutyerli@gmail.com>
2024-12-19 10:05:52 +0300

    Add hover color effect on mouse move to ProjectCards

---
+++ src/components/ProjectCard/HoverEffect.tsx
@@ -0,0 +1,26 @@
+import { type MotionValue, motion, useMotionTemplate } from 'framer-motion';
+
+export function HoverEffect({
+  mouseX,
+  mouseY,
+}: {
+  mouseX: MotionValue<number>;
+  mouseY: MotionValue<number>;
+}) {
+  let maskImage = useMotionTemplate`radial-gradient(180px at ${mouseX}px ${mouseY}px, white, transparent)`;
+  let style = { maskImage, WebkitMaskImage: maskImage };
+
+  return (
+    <div className="pointer-events-none">
+      <div className="absolute inset-0 rounded-2xl transition duration-300 [mask-image:linear-gradient(white,transparent)] group-hover:opacity-50"></div>
+      <motion.div
+        className="absolute inset-0 rounded-2xl bg-gradient-to-r from-zinc-200 to-zinc-300 opacity-0 transition duration-300 group-hover:opacity-50 dark:from-zinc-700 dark:to-zinc-800"
+        style={style}
+      />
+      <motion.div
+        className="absolute inset-0 rounded-2xl opacity-0 mix-blend-overlay transition duration-300 group-hover:opacity-100"
+        style={style}
+      ></motion.div>
+    </div>
+  );
+}
--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,10 +1,12 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import { formattedLinks } from './ProjectCard.helpers';
 import { spartan } from 'fonts';
 import clsx from 'clsx';
-import BgPattern from 'components/BgPattern/BgPattern';
-import ProjectCardBg from './ProjectCardBg';
+import { HoverEffect } from './HoverEffect';
+import { useMotionValue } from 'framer-motion';
 
 interface ProjectCardType {
   title: string;
@@ -27,19 +29,34 @@ export default function ProjectCard({
   className,
   freelance = false,
 }: ProjectCardType) {
+  let mouseX = useMotionValue(0);
+  let mouseY = useMotionValue(0);
+
+  function onMouseMove({
+    currentTarget,
+    clientX,
+    clientY,
+  }: React.MouseEvent<HTMLDivElement>) {
+    let { left, top } = currentTarget.getBoundingClientRect();
+    mouseX.set(clientX - left);
+    mouseY.set(clientY - top);
+  }
+
   return (
     <div
       className={clsx(
-        'flex flex-col relative',
+        'flex flex-col relative group',
         featured
           ? 'w-full md:text-center md:mb-20'
           : 'max-w-full md:max-w-[45%]',
         className
       )}
+      onMouseMove={onMouseMove}
+      key={title}
     >
       <div
         className={clsx(
-          'relative overflow-hidden rounded-lg shadow-md drop-shadow-md'
+          'relative overflow-hidden rounded-lg shadow-md drop-shadow-md z-10'
         )}
       >
         <Link href={formattedLinks(slug)}>
@@ -55,26 +72,28 @@ export default function ProjectCard({
           />
         </Link>
       </div>
-
-      <div
-        className={clsx(
-          'flex flex-col gap-3 max-w-96',
-          featured ? 'md:self-center' : 'self-start '
-        )}
-      >
-        <h2
-          className={`${spartan.className} text-3xl lg:text-4xl mt-6 tracking-tighter font-semibold text-zinc-800 dark:text-zinc-200`}
+      <div className="relative">
+        <div
+          className={clsx(
+            'flex flex-col gap-3 max-w-96',
+            featured ? 'md:self-center justify-self-center' : 'self-start '
+          )}
         >
-          {title}
-        </h2>
-        <p className="text-zinc-600 dark:text-zinc-400">{summary}</p>
-        <div className="flex justify-between w-full">
-          <Link href={formattedLinks(slug)}>Case Study</Link>
-          <Link href={liveLink} target="_blank">
-            Live Link
-          </Link>
+          <h2
+            className={`${spartan.className} text-3xl lg:text-4xl mt-6 tracking-tighter font-semibold text-zinc-800 dark:text-zinc-200`}
+          >
+            {title}
+          </h2>
+          <p className="text-zinc-600 dark:text-zinc-400">{summary}</p>
+          <div className="flex justify-between w-full">
+            <Link href={formattedLinks(slug)}>Case Study</Link>
+            <Link href={liveLink} target="_blank">
+              Live Link
+            </Link>
+          </div>
         </div>
       </div>
+      {!freelance && <HoverEffect mouseX={mouseX} mouseY={mouseY} />}
     </div>
   );
 }
