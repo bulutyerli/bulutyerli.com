1b52485e1b24844c8d54e87f1d31cc99cc5ff090
bulut yerli <bulutyerli@gmail.com>
2024-12-30 11:51:23 +0300

    Write tests for handleRoute helper

--- src/components/Header/Header.helpers.test.tsx
+++ src/components/Header/Header.helpers.test.tsx
@@ -1,30 +1,70 @@
-import { isActive } from './Header.helpers';
+import { useRouter } from 'next/navigation';
+import { handleRoute, isActive } from './Header.helpers';
 
 describe('Header helper functions tests', () => {
-  it('should return true when path and href same', () => {
-    const mockPath = 'contact';
-    const mockHref = 'contact';
+  describe('isActive helper function tests', () => {
+    it('should return true when path and href same', () => {
+      const mockPath = 'contact';
+      const mockHref = 'contact';
 
-    const result = isActive(mockPath, mockHref);
+      const result = isActive(mockPath, mockHref);
 
-    expect(result).toBeTruthy();
+      expect(result).toBeTruthy();
+    });
+
+    it('should return false when path and href are different', () => {
+      const mockPath = 'contact';
+      const mockHref = 'about-me';
+
+      const result = isActive(mockPath, mockHref);
+
+      expect(result).toBeFalsy();
+    });
+
+    it('should remove hash correctly and return true', () => {
+      const mockPath = 'contact';
+      const mockHref = '#contact';
+
+      const result = isActive(mockPath, mockHref);
+
+      expect(result).toBeTruthy();
+    });
   });
 
-  it('should return false when path and href are different', () => {
-    const mockPath = 'contact';
-    const mockHref = 'about-me';
+  describe('handleRoute helper function tests', () => {
+    const scrollIntoViewMock = jest.fn();
+    Element.prototype.scrollIntoView = scrollIntoViewMock;
 
-    const result = isActive(mockPath, mockHref);
+    const mockRouter = useRouter();
 
-    expect(result).toBeFalsy();
-  });
+    beforeEach(() => {
+      jest.clearAllMocks();
+      document.body.innerHTML = '<div id="about"></div>';
+    });
 
-  it('should remove hash correctly and return true', () => {
-    const mockPath = 'contact';
-    const mockHref = '#contact';
+    it('should ', () => {
+      const mockPathName = '/';
+      const mockHref = '#about';
 
-    const result = isActive(mockPath, mockHref);
+      console.log('Calling handleRoute');
 
-    expect(result).toBeTruthy();
+      handleRoute(mockPathName, mockHref, mockRouter);
+      expect(scrollIntoViewMock).toHaveBeenCalledWith({ behavior: 'smooth' });
+      expect(mockRouter.push).not.toHaveBeenCalled();
+    });
+
+    it('should push user to correct url if not at homepage', () => {
+      const mockPathName = '/projects/workwise';
+      const mockHref = '#about';
+
+      handleRoute(mockPathName, mockHref, mockRouter);
+
+      expect(scrollIntoViewMock).not.toHaveBeenCalledWith({
+        behavior: 'smooth',
+      });
+
+      expect(scrollIntoViewMock).not.toHaveBeenCalled();
+      expect(mockRouter.push).toHaveBeenCalledWith(`/${mockHref}`);
+    });
   });
 });
