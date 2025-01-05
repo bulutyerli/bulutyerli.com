8882b8dfc92bece926968c29f98835a797ea3bea
bulut yerli <bulutyerli@gmail.com>
2025-01-05 16:06:59 +0300

    Remove opacity animations on image component

--- src/components/Mdx/ImageComponent/ImageComponent.tsx
+++ src/components/Mdx/ImageComponent/ImageComponent.tsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx';
 import Image from 'next/image';
-import MotionWrapper from '../MotionWrapper/MotionWrapper';
 
 export interface Images {
   src: string;
@@ -17,26 +16,24 @@ export default function ImageComponent({
   className?: string;
 }) {
   return (
-    <MotionWrapper tag="div" duration={0.3} delay={0.3}>
-      <div className="my-10 flex w-full flex-wrap items-center justify-center gap-5 xl:justify-between">
-        {images.map((image, index) => {
-          const isEven = index % 2 === 0;
-          return (
-            <Image
-              key={image.src}
-              src={image.src}
-              alt={image.alt}
-              width={image.width}
-              height={image.height}
-              className={clsx(
-                'not-prose m-0 rounded-xl',
-                !isEven && 'lg:mt-24',
-                className,
-              )}
-            />
-          );
-        })}
-      </div>
-    </MotionWrapper>
+    <div className="my-10 flex w-full flex-wrap items-center justify-center gap-5 xl:justify-between">
+      {images.map((image, index) => {
+        const isEven = index % 2 === 0;
+        return (
+          <Image
+            key={image.src}
+            src={image.src}
+            alt={image.alt}
+            width={image.width}
+            height={image.height}
+            className={clsx(
+              'not-prose m-0 rounded-xl',
+              !isEven && 'lg:mt-24',
+              className,
+            )}
+          />
+        );
+      })}
+    </div>
   );
 }
